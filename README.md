6948f3528199cba2c1fe840f40171cae1fc22a3d
wxsm <wxsms@foxmail.com>
2021-12-30 08:29:14 +0800

    feat: allow skip mp3

--- README.md
+++ README.md
@@ -29,4 +29,6 @@ options:
     can be multi url split by comma (,).
 --author
     optional. file downloaded as title-author-date.mp3, default to up's name.
+--skip-mp3
+    skip the mp3 transform and keep downloaded file as video.
 ```
--- src/download2mp3.js
+++ src/download2mp3.js
@@ -1,10 +1,14 @@
 import { download2disk } from './download.js';
 import { flv2mp3 } from './flv2mp3.js';
 import fs from 'fs';
+import argv from './argv.js';
 
 export async function download2mp3 (url) {
   try {
     const filename = await download2disk(url);
+    if (argv['skip-mp3']) {
+      return;
+    }
     await flv2mp3(filename);
     fs.unlinkSync(filename);
   } catch (err) {
