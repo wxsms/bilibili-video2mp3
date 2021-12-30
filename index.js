b0ac18e52a73a5608e4a558d23e001da331e3281
wxsm <wxsms@foxmail.com>
2021-12-30 11:39:36 +0800

    fix: chunk pages in case of too much in one video

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import { getDataByUrl } from './src/getDataByUrl.js';
 import argv from './src/argv.js';
 import { help } from './src/help.js';
 import { createRequire } from 'module';
+import { chunk } from 'lodash-es';
 
 const require = createRequire(import.meta.url);
 const pkg = require('./package.json');
@@ -31,12 +32,16 @@ const pkg = require('./package.json');
 
   for (let url of urls) {
     url = url.trim();
-    console.log(url);
+    // console.log(url);
     const data = await getDataByUrl(url);
     // console.log(data);
     // console.log(`total threads: ${data.videoData.pages.length}`);
-    await Promise.all(data.videoData.pages.map((v, i) => {
-      return download2mp3(url.indexOf('p=') > 0 ? url.replace(/p=\d+/, `p=${i + 1}`) : `${url.replace(/\?.+/, '')}?p=${i + 1}`);
-    }));
+    const pages = data.videoData.pages.map((v, i) => {
+      return url.indexOf('p=') > 0 ? url.replace(/p=\d+/, `p=${i + 1}`) : `${url.replace(/\?.+/, '')}?p=${i + 1}`;
+    });
+    const pageChunks = chunk(pages, 10);
+    for (const c of pageChunks) {
+      await Promise.all(c.map(download2mp3));
+    }
   }
 })();
