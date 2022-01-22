97cde9f7bfaaf0c780cf688675646d9ef9330ad6
wxsm <wxsms@foxmail.com>
2022-01-22 16:59:10 +0800

    fix: progress bar align

--- src/progress.js
+++ src/progress.js
@@ -3,10 +3,11 @@ import Progress from 'multi-progress';
 const multi = new Progress(process.stderr);
 
 export function createProgressBar(index, title, total) {
-  return multi.newBar(`${index} ${title}\t[:bar] :percent :etas :status`, {
+  return multi.newBar(`${index} ${title}\t\t[:bar] :percent :etas :status`, {
     complete: '=',
     incomplete: ' ',
     width: 30,
     total: total,
+    renderThrottle: 1000,
   });
 }
