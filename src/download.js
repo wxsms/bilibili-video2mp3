201445fd46c45cf885acf23a9f65d894c7c80e3a
Ji Liang <liaji@microsoft.com>
2022-12-31 10:10:20 -0800

    fix issues

--- src/download.js
+++ src/download.js
@@ -70,14 +70,17 @@ export async function download(url, index) {
   // const aid = videoData.aid;
   const pid = data.p;
   const cid = pages[pid - 1].cid;
-  const title = (isSingle ? videoData.title : pages[pid - 1].part).replace(
+  const titleOld = (isSingle ? videoData.title : pages[pid - 1].part).replace(
     /\s/g,
     '-'
-  ).replace("/", "-");
+  );
+  const title = titleOld.replaceAll('/', '-');
+
   const date = new Date(videoData.ctime * 1000);
   const dateString = `${date.getFullYear()}-${
     date.getMonth() + 1
   }-${date.getDate()}`;
+  
   const author = videoData.owner.name.replace("/", "-");
   const filename = `${getName(index, title, author, dateString)}.flv`;
   
